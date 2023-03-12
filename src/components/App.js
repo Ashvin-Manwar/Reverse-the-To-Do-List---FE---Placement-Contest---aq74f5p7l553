e0b44da097ef166317bfdfb3a57827a3fa8c2bd5
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-03-12 15:39:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 
-function ToDo({todoid,createdAt}) {
-  return (<tr>
+function ToDo({todoId,createdAt}) {
+  return (
+    <tr>
     <td>
-      <p>{todoid}</p>
+      <p>{todoId}</p>
     </td>
     <td>
       <input placeholder="Enter the text here" />
@@ -12,14 +13,17 @@ function ToDo({todoid,createdAt}) {
     <td>
       <p>{createdAt}</p>
     </td>
-  </tr>)
+  </tr>
+)
 }
 
 function App() {
-  const [todos, setTodos] = useState([{
+  const [todos, setTodos] = useState([
+    {
     id: 'todo1',
     createdAt: '20:30',
-  }, {
+  }, 
+    {
     id: 'todo2',
     createdAt: '18:00',
   }
@@ -36,7 +40,7 @@ function App() {
        
 {
     todos.map((todo)=>(
-     <ToDo key={todo.id} todoid={todo.id} createdAt={todo.createdAt}/>)
+     <ToDo key={todo.id} todoId={todo.id} createdAt={todo.createdAt}/>)
 )}
      </tbody>
       </table>
